2e15e76457cd9547974ea51cd1e319161b5f596e
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-06-24 15:59:37 -0600

    Agregar archivos de proyecto.

---
+++ EJERCISIO VIDEO6/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EJERCISIO_VIDEO6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+        }
+    }
+}
